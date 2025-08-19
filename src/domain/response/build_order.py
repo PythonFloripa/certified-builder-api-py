e9d8e3d3e7afc8afe471d7f80fadbaf5efda5e3e
Maxson Almeida <maxsonferovante@gmail.com>
2025-08-19 16:42:03 -0300

    Update BuildOrderResponse model: Change existing_orders and new_orders types from List[int] to List[Any] to allow for more flexible data structures.

--- src/domain/response/build_order.py
+++ src/domain/response/build_order.py
@@ -1,9 +1,9 @@
 from pydantic import BaseModel, Field
-from typing import List
+from typing import List, Any
 from datetime import datetime
 
 class BuildOrderResponse(BaseModel):
     certificate_quantity: int
-    existing_orders: List[int]
-    new_orders: List[int]
+    existing_orders: List[Any]
+    new_orders: List[Any]
     processing_date: datetime = Field(default_factory=datetime.now)
