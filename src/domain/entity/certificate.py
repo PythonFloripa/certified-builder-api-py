6a89881103feada976087a6a3e277dbc6f4c81c7
Maxson Almeida <maxsonferovante@gmail.com>
2025-08-19 16:41:46 -0300

    Refactor Certificate model: Update success field to use Field for default value, change optional fields to required for order_id, order_date, product_id, product_name, certificate_details, certificate_logo, and certificate_background to ensure data integrity.

--- src/domain/entity/certificate.py
+++ src/domain/entity/certificate.py
@@ -4,17 +4,17 @@ import uuid
 
 class Certificate(BaseModel):
     id: uuid.UUID = Field(default_factory=uuid.uuid4)
-    success: Optional[bool] = False
+    success: Optional[bool] = Field(default=False)
     certificate_key: Optional[str] = None
     certificate_url: Optional[str] = None
     generated_date: Optional[str] = None
-    order_id: Optional[int]
-    order_date: Optional[str]
-    product_id: Optional[int]
-    product_name: Optional[str]
-    certificate_details: Optional[str]
-    certificate_logo: Optional[str]
-    certificate_background: Optional[str]
+    order_id: int
+    order_date: str
+    product_id: int
+    product_name: str
+    certificate_details: str
+    certificate_logo: str
+    certificate_background: str
     participant_email: Optional[str]
     participant_first_name: Optional[str]
     participant_last_name: Optional[str]
