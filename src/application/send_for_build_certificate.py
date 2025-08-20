ce891292fda17d966498dac0a2b078d43509b073
Maxson Almeida <maxsonferovante@gmail.com>
2025-08-20 20:39:58 -0300

    Refactor SendForBuildCertificate class: Simplify order processing by splitting orders into parts for SQS sending, enhancing efficiency and maintainability. Remove extensive docstrings to streamline code readability.

--- src/application/send_for_build_certificate.py
+++ src/application/send_for_build_certificate.py
@@ -10,20 +10,9 @@ logger.setLevel(logging.INFO)
 
 
 class SendForBuildCertificate:
-    """
-    Classe responsável por enviar ordens para construção de certificados.
-    Recebe SQSService via injeção de dependência para evitar importação circular.
-    """
-
     def __init__(self, sqs_service: SQSService):
-        """
-        Inicializa a classe com o serviço SQS injetado.
-        
-        Args:
-            sqs_service: Instância do SQSService para envio de mensagens
-        """
+        self.parts = 4
         self.sqs_service = sqs_service
-
 
     @retry(
             exceptions=Exception,
@@ -33,19 +22,21 @@ class SendForBuildCertificate:
             logger=logger
     )
     def execute(self, orders: List[TechOrdersResponse]):
-        logger.info(f"Sending {len(orders)} orders to build certificate.")
         processed_orders = self.__processed_orders_dict(orders)
-        self.__send_to_sqs(processed_orders)   
+        for part in processed_orders:
+            self.__send_to_sqs(part)   
 
 
-    def __processed_orders_dict(self, orders: List[TechOrdersResponse]) -> List[dict]:
-        logger.info(f"Processing {len(orders)} orders to build certificate.")
-        return [order.model_dump() for order in orders]
+    def __processed_orders_dict(self, orders: List[TechOrdersResponse]) -> List[List[dict]]:
+        parts = []
+        for i in range(0, len(orders), self.parts):
+            parts.append([order.model_dump() for order in orders[i:i+self.parts]])
+        return parts
     
     def __send_to_sqs(self, orders: List[dict]):
         try:
-            logger.info(f"Sending {len(orders)} orders to build certificate.")
-            # self.sqs_service.send_message(orders)
+            logger.info(f"Sending {len(orders)} orders to build certificate.")            
+            self.sqs_service.send_message(orders)
             logger.info(f"Orders sent to build certificate.")
         except Exception as e:
             logger.error(f"Error sending orders to build certificate: {e}")
