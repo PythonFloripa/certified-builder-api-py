a2ec55a1926893a9969ced738df5f93869625c4d
Maxson Almeida <maxsonferovante@gmail.com>
2025-08-25 15:57:29 -0300

    Enable validation in APIGatewayRestResolver: Update the instantiation of APIGatewayRestResolver to include enable_validation=True, enhancing request validation capabilities.

--- src/infrastructure/aws/api_gateway_restr_resolver.py
+++ src/infrastructure/aws/api_gateway_restr_resolver.py
@@ -1,5 +1,7 @@
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 
-app: APIGatewayRestResolver = APIGatewayRestResolver()
+app: APIGatewayRestResolver = APIGatewayRestResolver(
+    enable_validation=True,
+)
 
 
